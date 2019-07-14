4be344d4a25169a33be5efe245ddd90357523011
gy-ulbak96 <rbfl9611@gmail.com>
2019-07-14 22:20:47 +0900

    delete fail

--- mypagemain/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MypagemainConfig(AppConfig):
-    name = 'mypagemain'
--- mypagemain/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name="mypagemain"),
-]
