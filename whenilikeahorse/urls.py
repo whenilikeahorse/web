b1d2ca54aa0cf020760704c44fdf9ee50c478332
woneyhoney <48667913+woneyhoney@users.noreply.github.com>
2019-07-23 19:25:08 +0900

    이거 안되면 안됨 .. (#3)
    * add basic structure of main(including account)
    
    * geem g 1 ba bo
    
    * 진짜 이젠 되야 해..

--- whenilikeahorse/urls.py
+++ whenilikeahorse/urls.py
@@ -1,27 +1,15 @@
-"""whenilikeahorse URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+import main.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main.views.main, name='main'),
     path('', include('main.urls')),
     path('userQ&A/', include('QandA.urls')),
 # <<<<<<< HEAD
     path('mypage/',include('mypagemain.urls')),
+    path('account/', include('account.urls')),
 # =======
 # >>>>>>> [Configure rebuild & bootstrap add] 구조 변경 및 부트스트랩 적용
 ]
