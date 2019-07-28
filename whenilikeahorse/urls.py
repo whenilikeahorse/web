331c04547d1f5d837a863f869cf35962cba6201e
chiheon <qwaserdf234@naver.com>
2019-07-28 22:47:47 +0900

    [timeline base] timeline base update

--- whenilikeahorse/urls.py
+++ whenilikeahorse/urls.py
@@ -7,12 +7,6 @@ urlpatterns = [
     path('', main.views.main, name='main'),
     path('', include('main.urls')),
     path('userQ&A/', include('QandA.urls')),
-<<<<<<< HEAD
-=======
-# <<<<<<< HEAD
     path('mypage/',include('mypagemain.urls')),
     path('account/', include('account.urls')),
-# =======
-# >>>>>>> [Configure rebuild & bootstrap add] 구조 변경 및 부트스트랩 적용
->>>>>>> b0e73ad213edb707d433e0d6a1b261b27bbe2c16
 ]
